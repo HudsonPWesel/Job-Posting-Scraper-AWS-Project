db3c75a52545fcfeaa2c4f7dd0093a65d95f74f9
HudsonPWesel <cswyld7k@gmail.com>
2024-11-19 14:37:41 -0500

    Scrape indeed

--- scraper.py
+++ scraper.py
@@ -1,13 +1,12 @@
 import undetected_chromedriver as uc
-
-
-def main():
-    driver = uc.Chrome(use_subprocess=False)
-    driver.get("https://www.linkedin.com/jobs/")
-    print(driver)
-    # close the browser
-    driver.quit()
-
+from selenium.webdriver.common.by import By
 
 if __name__ == "__main__":
-    main()
+    options = uc.ChromeOptions()
+    options.headless = False
+    driver = uc.Chrome(use_subprocess=False, options=options)
+    driver.get(
+        "https://www.indeed.com/q-Penetration-Testing-Intern-jobs.html?vjk=3a8d1aa8176e6055")
+    job_title = driver.find_element(
+        By.XPATH, '//*[@id="jobsearch-ViewjobPaneWrapper"]/div/div[2]/div[2]/div[1]/div/div[2]/div[1]/h2/span')
+    print(job_title.text)
