e3086a4d55b87cf27b622c08f5cf9c560d9168f2
HudsonPWesel <cswyld7k@gmail.com>
2024-11-19 15:32:58 -0500

    Get each jobposting

--- scraper.py
+++ scraper.py
@@ -3,14 +3,13 @@ from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 
 '''
-    [X] Get page 
+    [X] Get page
     [X] Get the links for every post in the page
-    [ ] Click to render full panel
+    [ ] Click to reder full panel prepend https://indeed.com
     [ ] Get that data
-    [ ] Once complete write to file
-    [ ] Move to next page
-'''
+    [ ] Once compltee move to next page
 
+'''
 if __name__ == "__main__":
     options = uc.ChromeOptions()
     options.headless = False
@@ -23,4 +22,6 @@ if __name__ == "__main__":
     job_links = driver.find_elements(
         By.CSS_SELECTOR, '.jcs-JobTitle')  # Get all LI job postings links
 
-    # soup = BeautifulSoup(output, 'html.parser')
+    for link in job_links:
+        worker_driver = uc.Chrome(use_subprocess=False)
+        worker_driver.get(link.get_attribute("href"))
